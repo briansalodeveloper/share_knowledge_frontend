171767a597bb739471ea979b9bf5261f7176dba5
Brian <briansalo1997@gmail.com>
2022-08-26 14:28:50 +0800

    login feature

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import Login from './components/Login.vue'
+import router from './router'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/login', component: Login },
-      ]
-  })
 
 
 createApp(App).use(router).mount('#app')
