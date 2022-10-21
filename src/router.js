163aa92ff3f01877fab24a0b999c3dd5b2906df4
brian <briansalo1997@gmail.com>
2022-10-22 00:01:03 +0800

    add route guard/middleware in create-post url

--- src/router.js
+++ src/router.js
@@ -1,18 +1,33 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Login from './components/contents/authentication/Login.vue'
 import Register from './components/contents/authentication/Register.vue'
-import Home from './components/Container.vue'
+import Container from './components/Container.vue'
 import Facebook from './components/contents/authFacebook/Facebook.vue'
+import store from './vuex.js';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/login', name:'login', component: Login },
         { path: '/register', name:'register', component: Register },
-        { path: '/', name:'home', component: Home },
+        { path: '/', name:'home', component: Container },
         { path: '/callback/facebook', name:'callback_facebook', component: Facebook },
-        { path: '/create-post', name:'create_post', component: Home },
+        { path: '/create-post', name:'create_post', component: Container, meta: { requiresAuth: true } },
       ]
   })
+router.beforeEach(async (to, from, next) => {
 
+    if(to.meta.requiresAuth) {
+        await store.dispatch('checkTokenIfStillAuth')
+        if(store.state.token) {
+            next();
+        }else{
+            next({name:'login'});
+        } 
+    }else{
+        next();
+    }
+
+    
+});
 export default router;
--- src/vuex.js
+++ src/vuex.js
@@ -1,15 +1,29 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+let tokenId = localStorage.getItem("tokenId");
 
 export default createStore({
   state () {
     return {
-      token: ''
+      token: tokenId
     }
   },
   mutations: {
-    getToken (state, {tokenId}) {
+    getToken (state, tokenId) {
       state.token = tokenId;
     }
-  }
+  },
+  actions: {
+    async checkTokenIfStillAuth({commit}) {
+      let tokenId = localStorage.getItem("tokenId");
+        await axios.get('http://127.0.0.1:8000/api/checkAuth', {params:{token:tokenId}}).then((response) => {
+          
+        }).catch((error) => {
+          localStorage.removeItem('tokenId');
+          commit('getToken', null)  
+        });
+    }
+  },
+
 })
 
